333e5609aaf5ef5f234f3fe7a5a1e9ac4aab8b4b
mattalagala <matt15y@gmail.com>
2020-06-24 21:31:47 -0500

    Added 3 arrays to sort through input file

--- VerdeValleyVolleyball.java
+++ VerdeValleyVolleyball.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.io.FileNotFoundException;
+import java.lang.String;
+
 
 
 class Roster {
@@ -76,8 +80,9 @@ class VerdeValleyVolleyball {
             if (userSelect == 1) {
                 System.out.println("1111111111111111111");
                 System.out.println("Please provide roster file name: ");
-                String fileName = scnr.next();
-                openRoster(fileName);
+                String fileName = "roster1.txt";
+                // String fileName = scnr.next();
+                openRoster();
                 return userSelect;
 
             } else if (userSelect == 2) {
@@ -99,37 +104,132 @@ class VerdeValleyVolleyball {
         return userSelect;
 
     }
-    
-    public static Scanner openRoster (String fileName) throws IOException {
 
-        Scanner rosterData = new Scanner(new File(fileName));
+
+    
+  
+        public static void openFile (String fileName) throws FileNotFoundException {
+     
+            //creating File instance to reference text file in Java
+            File text = new File(fileName);
+         
+            //Creating Scanner instnace to read File in Java
+            Scanner scnr = new Scanner(text);
+         
+            //Reading each line of file using Scanner class
+            int lineNumber = 1;
+            while(scnr.hasNextLine()){
+                String line = scnr.nextLine();
+                System.out.println("line " + lineNumber + " :" + line);
+                lineNumber++;
+            }      
+       
+        }  
+     
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    public static String [][][] openRoster () throws FileNotFoundException {
+        
+        
+        Scanner rosterData = new Scanner(new File("roster1.txt"));
         int lineCount = 0;
         int count = 0;
         int newCount = 0;
-        int index = 0;
-        String [] myArray = new String[15];
-        while(rosterData.hasNext()){
-            lineCount++;
-            count+=1;                    
-            String currentLine = rosterData.nextLine();                    
-            // lineValue=Integer.parseInt(currentLine);
-            // sumDouble+=lineValue;
-            // sumOfSquares = lineValue*lineValue;
-            // totalOfSquares += sumOfSquares;
-            System.out.println(currentLine);
+        int i = 0;
+        int rows=20;
+        int cols = 20;
+        int j = 0;
+        int k =0;
+        String charLine = " ";
+        String charLine2 = " ";
+        String blockArray [] = new String [rows];
+        String totalArray [][][] = new String [20][20][20];
+        int first = 0;
+        
+        String currentLine = " ";
+        
+
+        while (rosterData.hasNextLine()) {
+            currentLine = rosterData.nextLine();
+            // System.out.println("   ~~~~~~~#@#$@#$@#   "+currentLine+"   $@#$@#$#@$~~~~~~");
+
+            blockArray[i] = currentLine;
+          
+            String fourLetter = blockArray[i].substring(blockArray[i].length()-4, blockArray[i].length());
+            totalArray[i][j][k] = fourLetter;
+            k++;
+            String eightLetter = blockArray[i].substring(blockArray[i].length()-8, blockArray[i].length());
+            totalArray[i][j][k] = eightLetter;
+            j++;
+            String nameLetter = blockArray[i].substring(first, blockArray[i].length()-8);
+            totalArray[i][j][k] = nameLetter;
+            i++;
             
 
-            if (newCount<myArray.length) {
-                
-                myArray[index] = currentLine;
-                System.out.println(myArray[index]);
-            }
-            index++;
-            newCount++;
-        } 
+            System.out.println(totalArray[i][j][k]+ "~~~~~~~~~~~~~");
+        }
+
         
-        System.out.println("~~~~~~~~~~~~~"+myArray[4]+"~~~~~~~~~~");
-        return rosterData;
+
+            // System.out.println(fourLetter+ "   ~~~~~~~#@#$@#$@#$@#$@#$#@$~~~~~~");
+            
+            // for (int j=0; j<myArray[i].length(); j++) {
+
+            //     String fourLetter = myArray[i].substring(i,i+4);
+
+            //     System.out.println(fourLetter+ "~~~~~~~#@#$@#$@#$@#$@#$#@$~~~~~~");
+
+            // }
+
+
+
+            
+            // for (i=0; i<=myArray.length; i++) {
+            //     // currentLine = rosterData.nextLine();
+            //     myArray[i] = currentLine;
+
+
+
+
+                // System.out.println(currentLine + "==========");
+            //     System.out.println(Arrays.toString(myArray)+ "++++++++++");
+                
+            // // }
+
+
+            // System.out.println(Arrays.toString(myArray)+ "++++++++ COMPLETE ARRAY ++++++++++");
+           
+
+            
+         
+        
+        // System.out.println(Arrays.toString(myArray2)+ "   2222222222222");
+        // System.out.println(myArray[0]+ "~~~~~~ARRAY WITH INDEX 0~~~~~~~");
+
+
+        return totalArray;
+          
+    }
+
+    public static void splitArray (String [][][] totalArray) {
+
+        System.out.println(Arrays.deepToString(totalArray)+ "~~~~~~~~~~~~~");
+
+        
+
 
     }
     
@@ -150,12 +250,20 @@ class VerdeValleyVolleyball {
 
     public static void displayTeams () {
         
-
+        // String names = " Pet, Lee, Tom, Edo";
+        // String[] splitNames = names.split(", ");
+     
+        // System.out.println(Arrays.toString(splitNames));
     }
+    
+
+	
 
     public static void main (String [] args) throws IOException{
         
-        chooseMenuItem();
+        String [][][] test = openRoster();
+        splitArray(test);
+     
         
         
 
