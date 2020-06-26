70b221a881859126586a5ce65dbf8ddd6b19edb7
mattalagala <matt15y@gmail.com>
2020-06-26 00:14:38 -0500

    Added blockers method to choose top 3 blockers

--- VerdeValleyVolleyball.java
+++ VerdeValleyVolleyball.java
@@ -43,18 +43,18 @@ public class VerdeValleyVolleyball {
         System.out.println("Welcome to Verde Valley");
         System.out.println("Please choose from the following menu");
         int userSelect = scnr.nextInt();
-        
+        ArrayList <player> playerArray = new ArrayList <player>();
         while (userSelect <=5) {
             
             if (userSelect == 1) {
                 System.out.println("1111111111111111111");
                 System.out.println("Please provide roster file name: ");
                 
-                openRoster();
+                playerArray = openRoster();
                 return userSelect;
 
             } else if (userSelect == 2) {
-                // chooseAttackers();
+                chooseAttackers(playerArray);
                 System.out.println(userSelect + "222222222222222222222");
  
                 return userSelect;
@@ -87,105 +87,40 @@ public class VerdeValleyVolleyball {
 
         ArrayList<player> eachPlayer = new ArrayList<player>();
 
-        
-        // eachPlayer.add(new player(name:: words[0] + " " + words[1], attack:: Double.parseDouble(words[2], block:: Double.parseDouble(words[3]))));
-
-
         while ((thisLine = file.readLine()) !=null) {
             
               String words[] = thisLine.split(" ");
-
-              
-
               eachPlayer.add(new player(words[0] + " " + words[1],Double.parseDouble(words[2]),Double.parseDouble(words[3])));
-              
-            //   eachPlayer[index].name = words[0] + " " + words[1];
-            //   eachPlayer[index].attack = Double.parseDouble(words[2]);
-            //   eachPlayer[index].block = Double.parseDouble(words[3]);
-
-            //   eachPlayer[index] = new player();
-            //   eachPlayer[index].name = words[0] + " " + words[1];
-            //   eachPlayer[index].attack = Double.parseDouble(words[2]);
-            //   eachPlayer[index].block = Double.parseDouble(words[3]);
-              
               index++;
-              System.out.println(Arrays.toString(words));
               
-                System.out.println(eachPlayer + "  ARRAYYY LIISTTTT ");
-            
-            
-            // System.out.println(Arrays.toString(eachPlayer));
+            // file.close()
 
-            // file.close();
-
-            
         }  
-        System.out.println(eachPlayer + "  ARRAYYY LIISTTTT ");
+        
         return eachPlayer;    
     }
 
     public static void chooseAttackers(ArrayList <player> newPlayer){
-
-        
-        // List<player> attackPlayer = new ArrayList <>();
 
         List<player> attackPlayer = newPlayer.stream()
                     .sorted(Comparator.comparing(person -> person.attack))
                     .collect(Collectors.toList());
                     Collections.reverse(attackPlayer);
                     
-        attackPlayer.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.attack+ " " + person.block));
-        // System.out.println(Arrays.toString(attackPlayer.get(0)) + "  WWWOOOOOOOWWW ");
-
-        // for(player current_cat : newPlayer){
-        //     if(current_cat.name != null){
-        //        current_cat = newPlayer[i];
-        //     }
-        //  }
-
-        // int count;
-        // count = newPlayer.length-1;
-        // System.out.println(count+"~~~~~~~~~~~");
-
-        // player temp_player = new player();
-        // for(int sort_iters = 0; sort_iters < 15; sort_iters++) {
-                 
-        //    for(int index = 0; index < 15-sort_iters-1; index++) {				
-        //       if(newPlayer[index].name.compareTo(newPlayer[index+1].name) > 0){ 
-        //          // Swap
-        //          temp_player = newPlayer[index];
-        //          newPlayer[index] = newPlayer[index+1];
-        //          newPlayer[index+1] = temp_player;
-        //       }
-        //    }
-        // }
-     
-
-
-
-
-
-
-
-
-        // for(int i=0;i<14;i++){
-        //     int j = 1;
-        //     if(newPlayer[i].attack > newPlayer[j].attack){
-        //         player temp = newPlayer[i];
-        //         newPlayer[i] = newPlayer[j];
-        //         newPlayer[j] = temp;
-        //         j++;
-        //     }
+        attackPlayer.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.attack));
         
-        // }
-        // System.out.println("~~~~~~~~~~~"+(newPlayer[15].name)+"~~~~~~~~~~~");
-        // System.out.println("~~~~~~~~~~~"+(newPlayer[15].attack)+"~~~~~~~~~~~");
         
     }
 
 
         
-    public static void openRoster (int userSelect) throws FileNotFoundException, IOException {
+    public static void chooseBlockers(ArrayList <player> newPlayer){
+        List<player> blockPlayers = newPlayer.stream()
+                    .sorted(Comparator.comparing(person -> person.block))
+                    .collect(Collectors.toList());
+                    Collections.reverse(blockPlayers);
+                    
+        blockPlayers.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.block));
         
         
            }   
