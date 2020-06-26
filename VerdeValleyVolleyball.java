42ccfd88fbbd12ee2e3643a58172b8b12aefd4ec
mattalagala <matt15y@gmail.com>
2020-06-25 23:58:11 -0500

    Added methods for userSelection

--- VerdeValleyVolleyball.java
+++ VerdeValleyVolleyball.java
@@ -1,73 +1,42 @@
-import java.util.Currency;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.io.FileNotFoundException;
+// import java.util.Currency;
+import java.util.*;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Comparator;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+// import java.io.FileOutputStream;
+// import java.io.PrintWriter;
+// import java.io.IOException;
+// import java.util.Arrays;
+// import java.io.FileNotFoundException;
 import java.lang.String;
 
-
-
-class Roster {
-    static Scanner scnr = new Scanner(System.in);
-
-
-    public static void chooseMenuItem () {
-
-        // System.out.println("Please choose from the following menu");
-        // int userSelect = scnr.input();
-
-        // System.out.println(userSelect);
-
-        // if(userSelect ==1) {
-        //     System.out.println("~~~~~~" + userSelect);
-        //     if (userSelect == 1) {
-        //         System.out.println("1111111111111111111");
-
-        //         userSelect--;
-
-        //     // } else if (userSelect.equals("2")) {
-        //     //     System.out.println("222222222222222222222");
-
-        //     // } else if (userSelect.equals("3")) {
-        //     //     System.out.println("333333333333333333333333");
-                
-        //     // } else if (userSelect.equals("4")) {
-        //     //     System.out.println("444444444444444444444444");
-                
-        //     // } else if (userSelect.equals("5")) {
-        //     //     System.out.println("555555555555555555555555555");
-                
-        //     }
-
-        // }
-    }
-
-    // public static void startMenu() {
-
-    //     if (userSelect.equals("1")) {
-
-    //     } else if (userSelect.equals("2")) {
-
-    //     } else if (userSelect.equals("3")) {
-            
-    //     } else if (userSelect.equals("4")) {
-            
-    //     } else if (userSelect.equals("5")) {
-            
-    //     }
-
-    // }
-
+class player {
+        
     
-}
+    String name;
+    double attack;
+    double block;
+    int selected;
 
-class VerdeValleyVolleyball {
-    static Scanner scnr = new Scanner(System.in);
+    player(String newName, double newAttack, double newBlock) {
+        name = newName;
+        attack = newAttack;
+        block = newBlock;
+        selected = 0;
+        }
+        
+    public double getAttack() {
+        return attack;
+        }
+}    
 
-    
+public class VerdeValleyVolleyball {
+
+    static Scanner scnr = new Scanner(System.in);   
+        
 
     public static int chooseMenuItem () throws IOException {
 
@@ -80,14 +49,14 @@ class VerdeValleyVolleyball {
             if (userSelect == 1) {
                 System.out.println("1111111111111111111");
                 System.out.println("Please provide roster file name: ");
-                String fileName = "roster1.txt";
-                // String fileName = scnr.next();
+                
                 openRoster();
                 return userSelect;
 
             } else if (userSelect == 2) {
+                // chooseAttackers();
                 System.out.println(userSelect + "222222222222222222222");
-
+ 
                 return userSelect;
             } else if (userSelect == 3) {
                 System.out.println("333333333333333333333333");
@@ -105,29 +74,92 @@ class VerdeValleyVolleyball {
 
     }
 
+    public static ArrayList <player> openRoster () throws IOException {
 
-    
-  
-        public static void openFile (String fileName) throws FileNotFoundException {
-     
-            //creating File instance to reference text file in Java
-            File text = new File(fileName);
-         
-            //Creating Scanner instnace to read File in Java
-            Scanner scnr = new Scanner(text);
-         
-            //Reading each line of file using Scanner class
-            int lineNumber = 1;
-            while(scnr.hasNextLine()){
-                String line = scnr.nextLine();
-                System.out.println("line " + lineNumber + " :" + line);
-                lineNumber++;
-            }      
-       
+        String fileName = "roster1.txt";
+                // String fileName = scnr.next();
+        BufferedReader file = new BufferedReader(new FileReader(fileName));
+
+        String thisLine;
+        int index=0;
+        // player eachPlayer[] = new player[25];
+
+
+        ArrayList<player> eachPlayer = new ArrayList<player>();
+
+        
+        // eachPlayer.add(new player(name:: words[0] + " " + words[1], attack:: Double.parseDouble(words[2], block:: Double.parseDouble(words[3]))));
+
+
+        while ((thisLine = file.readLine()) !=null) {
+            
+              String words[] = thisLine.split(" ");
+
+              
+
+              eachPlayer.add(new player(words[0] + " " + words[1],Double.parseDouble(words[2]),Double.parseDouble(words[3])));
+              
+            //   eachPlayer[index].name = words[0] + " " + words[1];
+            //   eachPlayer[index].attack = Double.parseDouble(words[2]);
+            //   eachPlayer[index].block = Double.parseDouble(words[3]);
+
+            //   eachPlayer[index] = new player();
+            //   eachPlayer[index].name = words[0] + " " + words[1];
+            //   eachPlayer[index].attack = Double.parseDouble(words[2]);
+            //   eachPlayer[index].block = Double.parseDouble(words[3]);
+              
+              index++;
+              System.out.println(Arrays.toString(words));
+              
+                System.out.println(eachPlayer + "  ARRAYYY LIISTTTT ");
+            
+            
+            // System.out.println(Arrays.toString(eachPlayer));
+
+            // file.close();
+
+            
         }  
+        System.out.println(eachPlayer + "  ARRAYYY LIISTTTT ");
+        return eachPlayer;    
+    }
+
+    public static void chooseAttackers(ArrayList <player> newPlayer){
+
+        
+        // List<player> attackPlayer = new ArrayList <>();
+
+        List<player> attackPlayer = newPlayer.stream()
+                    .sorted(Comparator.comparing(person -> person.attack))
+                    .collect(Collectors.toList());
+                    Collections.reverse(attackPlayer);
+                    
+        attackPlayer.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.attack+ " " + person.block));
+        // System.out.println(Arrays.toString(attackPlayer.get(0)) + "  WWWOOOOOOOWWW ");
+
+        // for(player current_cat : newPlayer){
+        //     if(current_cat.name != null){
+        //        current_cat = newPlayer[i];
+        //     }
+        //  }
+
+        // int count;
+        // count = newPlayer.length-1;
+        // System.out.println(count+"~~~~~~~~~~~");
+
+        // player temp_player = new player();
+        // for(int sort_iters = 0; sort_iters < 15; sort_iters++) {
+                 
+        //    for(int index = 0; index < 15-sort_iters-1; index++) {				
+        //       if(newPlayer[index].name.compareTo(newPlayer[index+1].name) > 0){ 
+        //          // Swap
+        //          temp_player = newPlayer[index];
+        //          newPlayer[index] = newPlayer[index+1];
+        //          newPlayer[index+1] = temp_player;
+        //       }
+        //    }
+        // }
      
-    
-    
 
 
 
@@ -136,179 +168,41 @@ class VerdeValleyVolleyball {
 
 
 
-
-
-
-
-    
-    public static String [][][] openRoster () throws FileNotFoundException {
+        // for(int i=0;i<14;i++){
+        //     int j = 1;
+        //     if(newPlayer[i].attack > newPlayer[j].attack){
+        //         player temp = newPlayer[i];
+        //         newPlayer[i] = newPlayer[j];
+        //         newPlayer[j] = temp;
+        //         j++;
+        //     }
         
+        // }
+        // System.out.println("~~~~~~~~~~~"+(newPlayer[15].name)+"~~~~~~~~~~~");
+        // System.out.println("~~~~~~~~~~~"+(newPlayer[15].attack)+"~~~~~~~~~~~");
         
-        Scanner rosterData = new Scanner(new File("roster1.txt"));
-        int lineCount = 0;
-        int count = 0;
-        int newCount = 0;
-        int i = 0;
-        int rows=20;
-        int cols = 20;
-        int j = 0;
-        int k =0;
-        int p = 0;
-        String charLine = " ";
-        String charLine2 = " ";
-        String blockArray [] = new String [rows];
-        String totalArray [][][] = new String [20][20][20];
-        int first = 0;
-        
-        String currentLine = " ";
-        
-
-        while (rosterData.hasNextLine()) {
-            currentLine = rosterData.nextLine();
-            // System.out.println("   ~~~~~~~#@#$@#$@#   "+currentLine+"   $@#$@#$#@$~~~~~~");
-
-            blockArray[i] = currentLine;
-          
-            String fourLetter = blockArray[i].substring(blockArray[i].length()-4, blockArray[i].length());
-            totalArray[i][j][k] = fourLetter;
-            k++;
-            String eightLetter = blockArray[i].substring(blockArray[i].length()-8, blockArray[i].length());
-            totalArray[i][j][k] = eightLetter;
-            j++;
-            String nameLetter = blockArray[i].substring(first, blockArray[i].length()-8);
-            totalArray[i][j][k] = nameLetter;
-            i++;
-            
-
-            System.out.println(totalArray[i][j][k]+ "~~~~~~~~~~~~~");
-        }
-
-        
-
-            // System.out.println(fourLetter+ "   ~~~~~~~#@#$@#$@#$@#$@#$#@$~~~~~~");
-            
-            // for (int j=0; j<myArray[i].length(); j++) {
-
-            //     String fourLetter = myArray[i].substring(i,i+4);
-            //
-
-            //     System.out.println(fourLetter+ "~~~~~~~#@#$@#$@#$@#$@#$#@$~~~~~~");
-
-            // }
-
-
-
-            
-            // for (i=0; i<=myArray.length; i++) {
-            //     // currentLine = rosterData.nextLine();
-            //     myArray[i] = currentLine;
-
-
-
-
-                // System.out.println(currentLine + "==========");
-            //     System.out.println(Arrays.toString(myArray)+ "++++++++++");
-                
-            // // }
-
-
-            // System.out.println(Arrays.toString(myArray)+ "++++++++ COMPLETE ARRAY ++++++++++");
-           
-
-            
-         
-        
-        // System.out.println(Arrays.toString(myArray2)+ "   2222222222222");
-        // System.out.println(myArray[0]+ "~~~~~~ARRAY WITH INDEX 0~~~~~~~");
-
-
-        return totalArray;
-          
     }
 
-    public static void splitArray (String [][][] totalArray) {
-
-        System.out.println(Arrays.deepToString(totalArray)+ "~~~~~~~~~~~~~");
 
         
-
-
-    }
-    
-    public static void topThreeAttackers () {
+    public static void openRoster (int userSelect) throws FileNotFoundException, IOException {
         
-
-    }
-    
-    public static void topThreeBlockers () {
         
-
-    }
-    
-    public static void makeTeams () {
-        
-
-    }
-
-    public static void displayTeams () {
-        
-        // String names = " Pet, Lee, Tom, Edo";
-        // String[] splitNames = names.split(", ");
-     
-        // System.out.println(Arrays.toString(splitNames));
-    }
+           }   
+       
     
 
 	
 
-    public static void main (String [] args) throws IOException{
+public static void main (String [] args) throws IOException{
+    
+    int userSelect = chooseMenuItem();
+    chooseAttackers(openRoster());
+    // openRoster(userSelect);
         
-        String [][][] test = openRoster();
-        splitArray(test);
-     
         
-        
-
 
     }
 
-}
-    
+}   
 
-
-// public static long display_problem(long problem_type) {
-             
-//     long correct_answer = 0;
-//      // Loop to iterate through the users selection   
-//      while (problem_type <= 4 ) {
-        
-//         if (problem_type == 1) {
-//             correct_answer = solve_y();
-//             return correct_answer;
-//         } else if (problem_type == 2) {
-//             correct_answer = solve_m();
-//             return correct_answer;
-//             }
-//           else if (problem_type == 3) {
-//             correct_answer = solve_b();
-//             return correct_answer;
-//             }
-//           else if (problem_type == 4) {
-//             System.out.println("Goodbye!");
-//             break;
-//             }          
-//     return correct_answer; 
-//         }
-//     //Required return statement for method
-//     return  correct_answer;        
-// }
-
-//  //Method to get user's selection
-// public static long which_problem() {
-//     //Gets user answer by calling scnr.nextInt() and saving to user_answer
-//     System.out.println("Select 1 to Solve for Y, 2 to Solve for M, 3 to Solve for B and 4 to quit!" );    
-//     int problem_type = scnr.nextInt();
-        
-//     return problem_type;
-        
-// }
