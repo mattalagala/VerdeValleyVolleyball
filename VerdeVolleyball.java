3b13091de7f9e005a6ee5817564a05739d1c09f0
mattalagala <matt15y@gmail.com>
2020-06-27 12:40:56 -0500

    Updated Menu Item to not break out of Menu loop after user selects 4

--- VerdeVolleyball.java
+++ VerdeVolleyball.java
@@ -60,6 +60,12 @@ public class VerdeVolleyball {
     static Scanner scnr = new Scanner(System.in);   
     boolean isLoaded = false;
     ArrayList<Player> eachPlayer = new ArrayList<Player>();
+
+    
+    int teamSize;
+    int numberOfTeams;
+    int attackerAndBlockerSize; 
+    
     
     public void openRoster () throws IOException {
 
@@ -84,49 +90,83 @@ public class VerdeVolleyball {
             // file.close()
 
         }  
-        
+        teamSize=eachPlayer.size();
+        numberOfTeams = teamSize / 6;
+        attackerAndBlockerSize = teamSize/2;
         System.out.println("Roster Loaded!");
+        System.out.println("THIS IS IT    >>" + numberOfTeams);
         chooseMenuItem();  
     }
 
-    public void chooseAttackers(ArrayList <Player> newPlayer) throws IOException {
+    public void chooseAttackers(ArrayList <Player> attackPlayers) throws IOException {
         System.out.println("Choose Attackers Initiated");
-        List<Player> attackPlayer = newPlayer.stream()
+        List<Player> attackers = attackPlayers.stream()
                     .sorted(Comparator.comparing(person -> person.attack))
                     .collect(Collectors.toList());
-                    Collections.reverse(attackPlayer);
-                    
-        attackPlayer.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.attack));        
+                    Collections.reverse(attackers);                    
+        attackers.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.attack));
+        
+        //This is for Scrimmage Teams
+        // List<Player> attackersForScrimmage = attackPlayers.stream()
+        //             .sorted(Comparator.comparing(person -> person.attack))
+        //             .collect(Collectors.toList());
+        //             Collections.reverse(attackersForScrimmage);                    
+        // attackersForScrimmage.stream().limit(attackerAndBlockerSize).forEach(person-> System.out.println("THIS IS SCRIMMAGE ATTACKERS "+person.name + " " + person.attack));     
         
         chooseMenuItem();
     }
 
-    public void chooseBlockers(ArrayList <Player> nePlayer) throws IOException {
-        List<Player> blockPlayers = nePlayer.stream()
+    public void chooseBlockers(ArrayList <Player> blockPlayers) throws IOException {
+        List<Player> blockers = blockPlayers.stream()
                     .sorted(Comparator.comparing(person -> person.block))
                     .collect(Collectors.toList());
-                    Collections.reverse(blockPlayers);
-                    
-        blockPlayers.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.block));        
+                    Collections.reverse(blockers);                    
+        blockers.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.block));
+        
+        // List<Player> blockersForScrimmage = blockPlayers.stream()
+        //             .sorted(Comparator.comparing(person -> person.block))
+        //             .collect(Collectors.toList());
+        //             Collections.reverse(blockersForScrimmage);                    
+        // blockersForScrimmage.stream().limit(attackerAndBlockerSize).forEach(person-> System.out.println(person.name + " " + person.block)); 
+        
         chooseMenuItem();
     }   
 
-    public static void displayTeams(ArrayList <Player> teamPlayer){
+    public void displayTeams(ArrayList <Player> teamPlayer) throws IOException{
         List<Player> teams = teamPlayer.stream()
                     .sorted(Comparator.comparing(person -> person.block))
                     .collect(Collectors.toList());
                     Collections.reverse(teams);
                     
         teams.stream().limit(3).forEach(person-> System.out.println(person.name + " " + person.block));
+
+        // Attackers for Scrimmage
+
+        List<Player> attackersForScrimmage = teamPlayer.stream()
+                    .sorted(Comparator.comparing(person -> person.attack))
+                    .collect(Collectors.toList());
+                    Collections.reverse(attackersForScrimmage);                    
+        attackersForScrimmage.stream().limit(attackerAndBlockerSize).forEach(person-> System.out.println("THIS IS SCRIMMAGE ATTACKERS "+person.name + " " + person.attack)); 
+
+        //Blockers for Scrimmage
+
+        List<Player> blockersForScrimmage = teamPlayer.stream()
+                    .sorted(Comparator.comparing(person -> person.block))
+                    .collect(Collectors.toList());
+                    Collections.reverse(blockersForScrimmage);                    
+        blockersForScrimmage.stream().limit(attackerAndBlockerSize).forEach(person-> System.out.println(person.name + " " + person.block)); 
+
+        attackersForScrimmage.subList(0, teamSize/2);
+        blockersForScrimmage.subList(0, teamSize/2);
         
-        int size = teams.size();
-
-
-        int numberOfTeams = 19 / 6;
-       
-        System.out.println(numberOfTeams + "~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("SCRIMMAGE TEAM 1");
+        System.out.println(blockersForScrimmage.subList(0, 3));
+        System.out.println(attackersForScrimmage.subList(4, 6));
+        System.out.println("SCRIMMAGE TEAM 2");
+        System.out.println(blockersForScrimmage.subList(4, 6));
+        System.out.println(attackersForScrimmage.subList(0, 3));
         
-
+        chooseMenuItem();
     } 
 
     public int chooseMenuItem () throws IOException {
@@ -138,7 +178,7 @@ public class VerdeVolleyball {
         System.out.println("3) List top 3 blockers");
         System.out.println("4) Make and display scrimmage teams");
         System.out.println("5) Quit" + "\n");
-        System.out.println("Please choose from the following menu" + "\n");
+        System.out.println("Please choose from the following menu" + "\n"  + ">> ");
         int userSelect = scnr.nextInt();
         selectMenuItem(userSelect);
         return userSelect;
@@ -153,8 +193,7 @@ public class VerdeVolleyball {
 
         
         if (userSelect ==1) {
-            isLoaded = true;
-            System.out.println(isLoaded + "   NOOOOWWW THIS IS THE STATUS OF isLoaded");
+            isLoaded = true;            
             openRoster();
         } else if (isLoaded && userSelect == 2) {
             System.out.println(isLoaded + "THIS IS THE STATUS OF isLoaded in menu item 2");
@@ -167,68 +206,20 @@ public class VerdeVolleyball {
             displayTeams(eachPlayer);
         } else if (userSelect !=5) {
             System.out.println(isLoaded + "THIS IS THE STATUS OF isLoaded in menu item !5");
-            System.out.println("You must first select 1 to choose a roster file: " + "\n");
+            System.out.println("You must first select 1 to choose a roster file: " + "\n" + ">> ");
             chooseMenuItem();
         } else if (userSelect ==5) {
             return;
         }
     }
-	
+	public ArrayList <Player> dummyMethod () throws IOException {
+
+       return eachPlayer;
+    }
 
 public static void main (String [] args) throws IOException{
-    VerdeVolleyball verdeClass = new VerdeVolleyball();
+    VerdeVolleyball verdeClass = new VerdeVolleyball();   
 
     verdeClass.chooseMenuItem();
-    
-    // int test = chooseMenuItem();
-    // selectMenuItem(test);
-
-
-    // chooseAttackers(openRoster());
-    // chooseBlockers(openRoster());
-    // displayTeams(openRoster());
-    
-    
-
-    // boolean loaded = false;
-    // int userSelect = chooseMenuItem();
-
-          
-    
-    // while (userSelect<5){
-    //     ArrayList <Player> test;  
-
-    //     if (userSelect ==1) {
-    //         System.out.println("ITS HERE ~~~~~~");
-    //         test = openRoster();        
-    //         loaded = true;
-    //         chooseMenuItem();
-            
-    //     } else if (userSelect == 2 && loaded) {
-    //         chooseAttackers(test);
-
-            
-            
-    //     } else if (userSelect == 3 && loaded) {
-    //         chooseAttackers(openRoster());
-            
-            
-    //     } else if (userSelect ==4 && loaded) {
-    //         chooseAttackers(openRoster());
-            
-            
-    //     } else {
-    //         System.out.println(" YOU MUST LOAD ROSTER FIRST ");
-    //         userSelect = chooseMenuItem();           
-    //     }
-
-    // }
-    
-    // // openRoster(userSelect);
-        
-        
-
-    // }
-
 }   
 }
